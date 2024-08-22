ae65c40ff3be4436cee2a6b278ffcbce8215fa9f
dnaals <swm8793@naver.com>
2024-08-22 23:25:43 +0900

    내용변경

--- src/comp/Skill.jsx
+++ src/comp/Skill.jsx
@@ -14,7 +14,7 @@ function Skill(props) {
         </div>
         <div className="skill_explain">
             <h2>What I can do with</h2>
-            <p>웹 표준을 주수하며 누구에게나 만족스러운 사용자 경험을 제공하는 
+            <p>웹 표준을 준수하며 누구에게나 만족스러운 사용자 경험을 제공하는 
             마크업을 구현하고 SASS를 활용하여 유지보수 가능한 반응형 웹사이트를 제작합니다.
             React, Next.JS 등의 프레임 워크를 이해하고 프로젝트에 활용할 수 있으며 MongoDB를 사용해 데이터 관리를 해본 경험이 있습니다.
             </p>
